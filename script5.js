897cb06d338ff69d74c971f299c2e05e609cae7d
Ezequiel Gonzalez <zequi14@hotmail.com>
2021-12-02 12:58:01 +0100

    Latin Pig

---
+++ script5.js
@@ -0,0 +1,59 @@
+// Pig Latin
+// Pig Latin is a way of altering English Words. The rules are as follows:
+
+// - If a word begins with a consonant, take the first consonant or consonant cluster, move it to the end of the word, and add ay to it.
+
+// - If a word begins with a vowel, just add way at the end.
+
+function translatePigLatin(str) {
+    let vowels = ['a','e','i','o','u'];
+    let index = 0;
+
+    if(vowels.indexOf(str[index]) !==-1){
+        return str + "way";
+    }
+    for(index =1; index < str.length; index++){
+        if(vowels.indexOf(str[index]) !==-1){
+            break;
+        }
+    }
+    console.log(index)
+    return str.slice(index) + str.slice(0,index) + "ay";
+
+    
+  }
+  
+  console.log(translatePigLatin("ramses"));
+
+  //Using Regular Expresions:
+  function translatePigLatin(str) {
+    let consonantRegex = /^[^aeiou]+/;
+    let myConsonants = str.match(consonantRegex);
+    return myConsonants !== null
+      ? str
+          .replace(consonantRegex, "")
+          .concat(myConsonants)
+          .concat("ay")
+      : str.concat("way");
+  }
+  
+  //Other way:
+  function translatePigLatin(str) {
+    // Create variables to be used
+    var pigLatin = "";
+    var regex = /[aeiou]/gi;
+  
+    // Check if the first character is a vowel
+    if (str[0].match(regex)) {
+      pigLatin = str + "way";
+    } else if (str.match(regex) === null) {
+      // Check if the string contains only consonants
+      pigLatin = str + "ay";
+    } else {
+      // Find how many consonants before the first vowel.
+      var vowelIndice = str.indexOf(str.match(regex)[0]);
+  
+      // Take the string from the first vowel to the last char
+      // then add the consonants that were previously omitted and add the ending.
+      pigLatin = str.substr(vowelIndice) + str.substr(0, vowelIndice) + "ay";
+    }}
