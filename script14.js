1aac3abe85beeb35c4352dea5fa90c552dcf3c15
Ezequiel Gonzalez <zequi14@hotmail.com>
2021-12-04 19:59:02 +0100

    convert roman

---
+++ script14.js
@@ -0,0 +1,18 @@
+// Roman Numeral Converter
+// Convert the given number into a roman numeral.
+
+function convertToRoman(number){
+
+    var lookup = {M:1000,CM:900,D:500,CD:400,C:100,XC:90,L:50,XL:40,X:10,IX:9,V:5,IV:4,I:1}
+    let roman = '';
+    for (key in lookup ) {
+      while ( number >= lookup[key] ) {
+        roman += key;
+        number -= lookup[key];
+      }
+    }
+    return roman;
+  }
+  
+  let result = convertToRoman(2504)
+  
